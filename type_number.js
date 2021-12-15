ca87d0acc0e5681c02c1113ccbb65a3b9ba2dee6
ViniciusK9 <viniciuskoncicoski@gmail.com>
2021-12-15 09:28:16 -0300

    Adicionando primeiro arquivo .js sobre tipos

---
+++ type_number.js
@@ -0,0 +1,45 @@
+// tipo inteiro
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const soma = primeiroNumero + segundoNumero;
+
+console.log(soma)
+
+// tipo flutuante
+
+const num_1 = 5.2;
+const num_2 = 1.6;
+
+const subtracao = num_1 - num_2;
+
+console.log(subtracao);
+
+// NaN -> Not a number 
+
+const palavra = "oi";
+
+const tentativaDeSoma = palavra + segundoNumero;
+
+console.log(tentativaDeSoma);
+
+const tentativaDeMultiplicacao = palavra * segundoNumero;
+
+console.log(tentativaDeMultiplicacao);
+
+// area de teste 
+
+const num = 1;
+const num_3 = 0;
+
+num_4 = 3.612734861782;
+num_5 = 3.612734861782;
+
+const operacao = num / num_3;
+
+num_4 = Math.round(num_4);
+
+console.log(operacao);
+console.log(num_4);
+console.log(num_5.toFixed(2));
