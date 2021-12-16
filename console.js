6d2805decef4f6cfd2fc5c1b00357cd470cc9fe7
ViniciusK9 <viniciuskoncicoski@gmail.com>
2021-12-16 11:32:06 -0300

    Adicionando arquivos sobre erros, tipagem e console

---
+++ console.js
@@ -0,0 +1,18 @@
+// log -> registro
+
+const minhaVar = true;
+
+console.log(123);
+console.log('meu texto');
+console.log(minhaVar);
+
+// tratamento de erro
+
+console.error('Deu erro!') // para exibir mensagem de erro.
+
+console.table() // para visualizar de forma mais organizada informações tabulares.
+
+console.time() // para temporizar período que uma operação de código leva para
+console.timeEnd() // ser iniciada e concluída.
+
+console.trace() // para exibir a stacktrace de todos os pontos por onde o código executado passou durante a execução.
---
+++ tipagem.js
@@ -0,0 +1,21 @@
+// tipagem dinâmica
+
+let variavel;
+console.log('valor: ',variavel);
+console.log('tipo: ',typeof(variavel))
+
+variavel = 123;
+console.log('valor: ',variavel);
+console.log('tipo: ',typeof(variavel))
+
+variavel = null;
+console.log('valor: ',variavel);
+console.log('tipo: ',typeof(variavel))
+
+variavel = 'texto';
+console.log('valor: ',variavel);
+console.log('tipo: ',typeof(variavel))
+
+variavel = true;
+console.log('valor: ',variavel);
+console.log('tipo: ',typeof(variavel))
