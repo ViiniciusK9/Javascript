168809c4aaeb2b3f196ebdc931b7894da07655bc
ViniciusK9 <viniciuskoncicoski@gmail.com>
2021-12-20 08:36:42 -0300

    adicionando arquivos sobre criar e manipular arrays

---
+++ array_js/adicionando_elementos.js
@@ -0,0 +1,15 @@
+// adicionando elementos em array
+
+const notas = [7.6, 9.3, 7.5, 8.6]
+
+console.log(notas)
+console.log("Tamanho da lista: ",notas.length)
+
+notas.push(7) // adiciona na ultima posição do array
+
+console.log(notas)
+console.log("Tamanho da lista: ",notas.length)
+
+let media = (notas[0] + notas[1] + notas[2] + notas[3] + notas[4]) / notas.length
+
+console.log(media)
---
+++ array_js/media.js
@@ -0,0 +1,11 @@
+// array -> const, let ou var nomeDoArray = [elementos separados por virgula]
+
+const notas = [10, 7.5, 4.5, 9.3]
+
+let media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length
+
+console.log(media)
+
+
+const nums = [50, 43, 12, 98];
+console.log(nums.length)
