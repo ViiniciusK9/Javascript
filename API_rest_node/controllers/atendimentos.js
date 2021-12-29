8d4ff485b9fbf6a0f4071e4a2ff81c7b380813ce
ViniciusK9 <viniciuskoncicoski@gmail.com>
2021-12-29 10:20:02 -0300

    adding error handling, dates, data validation and http status

--- API_rest_node/controllers/atendimentos.js
+++ API_rest_node/controllers/atendimentos.js
@@ -6,7 +6,7 @@ module.exports = app => {
     app.post('/atendimentos', (req, res) => {
         const atendimento = req.body
  
-        Atendimento.adiciona(atendimento)
-        res.send("Você está na rota de atendimentoes e está realizando um POST.")
+        Atendimento.adiciona(atendimento, res)
+        
     }) 
 }
