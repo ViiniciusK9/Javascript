3aca6dc185904a6c5daea7dc82a89805a482f7e4
ViniciusK9 <viniciuskoncicoski@gmail.com>
2021-12-29 09:07:31 -0300

    Adding files for mysql configuration, creating tables and saving data in tables

--- API_rest_node/controllers/atendimentos.js
+++ API_rest_node/controllers/atendimentos.js
@@ -1,8 +1,12 @@
+const Atendimento = require('../models/atendimentos')
+
 module.exports = app => {
     app.get('/atendimentos', (req, res) => res.send("Você está na rota de atendimentos e você está realizando um GET."))
 
     app.post('/atendimentos', (req, res) => {
-        console.log(req.body)
+        const atendimento = req.body
+ 
+        Atendimento.adiciona(atendimento)
         res.send("Você está na rota de atendimentoes e está realizando um POST.")
     }) 
 }
