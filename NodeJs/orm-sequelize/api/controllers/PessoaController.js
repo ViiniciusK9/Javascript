ad77ad424c2b9341084b0ef3a0a2bf7d0cc0730b
ViniciusK9 <viniciuskoncicoski@gmail.com>
2022-01-03 16:05:55 -0300

    creating crud with sequelize and creating routes for operations

--- NodeJs/orm-sequelize/api/controllers/PessoaController.js
+++ NodeJs/orm-sequelize/api/controllers/PessoaController.js
@@ -1,11 +1,75 @@
 const database = require('../models')
 
 class PessoaController {
+    //listar todos os registros
     static async pegaTodasAsPessoas(req, res){
         try{
             const todasAsPessoas = await database.Pessoas.findAll()
             return res.status(200).json(todasAsPessoas)
         } catch (error){
+            return res.status(500).json(error.message)
+        }
+    }
+
+    // listar registro com id específico
+    static async pegaPessoaId(req, res){
+        const { id } = req.params
+        try{
+            const pessoaId = await database.Pessoas.findOne({
+                where: {
+                    id: parseInt(id)
+                }
+            })
+            return res.status(200).json(pessoaId)
+        } catch (error){
+            return res.status(500).json(error.message)
+        }
+    }
+
+    // criar um novo registro 
+    static async criaPessoa(req, res) {
+        const novaPessoa = req.body
+        try {
+            const novaPessoaCriada = await database.Pessoas.create(novaPessoa)
+            return res.status(200).json(novaPessoaCriada)
+        }catch (error) {
+            return req.status(500).json(error.message)
+        }
+    }
+
+    // atualizar um registro
+    static async atualizaPessoa(req, res) {
+        const novasInfos = req.body
+        const { id } = req.params
+        try{
+            await database.Pessoas.update(novasInfos, {
+                where: {
+                    id: parseInt(id)
+                }
+            })
+            const pessoaAtualizada = await database.Pessoas.findOne({
+                where: {
+                    id: parseInt(id)
+                }
+            })
+            return res.status(200).json(pessoaAtualizada)
+        }catch (error){
+            return res.status(500).json(error.message)
+        }
+    }
+
+    // deletar um registro
+    static async apagaPessoa(req, res) {
+        const { id } = req.params
+        try{
+            await database.Pessoas.destroy({
+                where: {
+                    id: parseInt(id)
+                }
+            })
+            return res.status(200).json({ mensagem: `id ${id} deletado`})
+
+        }catch (error){
             return res.status(500).json(error.message)
         }
     }
--- NodeJs/orm-sequelize/api/routes/pessoasRoute.js
+++ NodeJs/orm-sequelize/api/routes/pessoasRoute.js
@@ -5,4 +5,12 @@ const router = Router()
 
 router.get('/pessoas', PessoaController.pegaTodasAsPessoas)
 
+router.get('/pessoas/:id', PessoaController.pegaPessoaId)
+
+router.post('/pessoas', PessoaController.criaPessoa)
+
+router.put('/pessoas/:id', PessoaController.atualizaPessoa)
+
+router.delete('/pessoas/:id', PessoaController.apagaPessoa)
+
 module.exports = router
